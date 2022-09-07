4684e446bda371af41430904c3a5aa7a4fd5555a
H. Jin <jinh@anl.gov>
2022-09-07 13:46:56 -0500

    add utils functions

--- deepHyp_scripts/deepHyp_v1.py
+++ deepHyp_scripts/deepHyp_v1.py
@@ -1,68 +1,70 @@
-import ray
 import json
-import pandas as pd
 from functools import partial
+
+import numpy as np
+import pandas as pd
+import ray
 import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.nn import Linear
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import random_split
-from torch import nn
 from torch_geometric.loader import DataLoader
+from torch_geometric.nn import GCNConv, SAGEConv, global_mean_pool
 
 
-def load_data():   
-    from torch_geometric.data import Data
+def load_data():
     import pickle
-    with open('graph_all_.pkl','rb') as f:
+
+    from torch_geometric.data import Data
+    with open('graph_all_.pkl', 'rb') as f:
         graphs = pickle.load(f)
 
-    import numpy as np
     y_list = []
     for gr in graphs:
         y_list.append(gr['y'])
     # print(min(y_list))
     # print(max(y_list))
-    # print(np.unique(np.array(y_list), return_counts=True))  
-    datasets=[]
-    import numpy
+    # print(np.unique(np.array(y_list), return_counts=True))
+    datasets = []
+
     for element in graphs:
-        gx = torch.tensor(numpy.array(element['x']) ) 
-        ge =torch.tensor(numpy.array(element['edge_index']) ).T
-        gy =torch.tensor(numpy.array(element['y']).reshape([-1]))
+        gx = torch.tensor(np.array(element['x']))
+        ge = torch.tensor(np.array(element['edge_index'])).T
+        gy = torch.tensor(np.array(element['y']).reshape([-1]))
         #print(gx.shape, ge.shape, gy.shape)
         # print(gy)
         v_min, v_max = gx.min(), gx.max()
         new_min, new_max = 0, 1
-        gx = (gx - v_min)/(v_max - v_min)*(new_max - new_min) + new_min
+        gx = (gx - v_min) / (v_max - v_min) * (new_max - new_min) + new_min
         # print(gx.min(), gx.max())
-        datasets.append( Data(x=gx, edge_index=ge, y=gy) )
+        datasets.append(Data(x=gx, edge_index=ge, y=gy))
     return datasets
 
+
 def run(config: dict):
-    import torch
-    from torch.nn import Linear
-    import torch.nn.functional as F
-    from torch_geometric.nn import  SAGEConv, GCNConv
-    from torch_geometric.nn import global_mean_pool
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     class GCN(torch.nn.Module):
         def __init__(self, hidden_channels, drop_rate, hidden, layer_index):
             super(GCN, self).__init__()
             layers = [SAGEConv, GCNConv]
             torch.manual_seed(12345)
             self.conv1 = layers[layer_index](14, hidden_channels).to(device)
-            self.conv2=[]
+            self.conv2 = []
             for i in range(hidden):
-                self.conv2.append(layers[layer_index](hidden_channels, hidden_channels).to(device) )
+                self.conv2.append(layers[layer_index](hidden_channels, hidden_channels).to(device))
             self.conv3 = layers[layer_index](hidden_channels, hidden_channels).to(device)
-            self.drop=drop_rate
-            self.lins=[]
+            self.drop = drop_rate
+            self.lins = []
             for i in range(hidden):
-                self.conv2.append( Linear(hidden_channels, hidden_channels).to(device) )
+                self.conv2.append(Linear(hidden_channels, hidden_channels).to(device))
             self.lin = Linear(hidden_channels, 4).to(device)
 
         def forward(self, x, edge_index, batch):
-            # 1. Obtain node embeddings 
+            # 1. Obtain node embeddings
             x = self.conv1(x, edge_index)
             x = x.relu()
 
@@ -82,12 +84,13 @@ def run(config: dict):
             return x
 
     # from IPython.display import Javascript
-    # display(Javascript('''google.colab.output.setIframeHeight(0, true, {maxHeight: 300})'''))     
+    # display(Javascript('''google.colab.output.setIframeHeight(0, true, {maxHeight: 300})'''))
     def train(model, criterion, optimizer, loader):
         model.train()
         for data in loader:  # Iterate in batches over the training dataset.
             optimizer.zero_grad()  # Clear gradients.
-            out = model(data.x.float().to(device), data.edge_index.to(device), data.batch.to(device))  # Perform a single forward pass.
+            # Perform a single forward pass.
+            out = model(data.x.float().to(device), data.edge_index.to(device), data.batch.to(device))
             loss = criterion(out, data.y.to(device))  # Compute the loss.
             loss.backward()  # Derive gradients.
             optimizer.step()  # Update parameters based on gradients.
@@ -96,37 +99,36 @@ def run(config: dict):
         model.eval()
         correct = 0
         for data in loader:  # Iterate in batches over the training/test dataset.
-            out = model(data.x.float().to(device),\
-                  data.edge_index.to(device),\
-                  data.batch.to(device))  
+            out = model(data.x.float().to(device),
+                        data.edge_index.to(device),
+                        data.batch.to(device))
             pred = out.argmax(dim=1).detach().cpu()  # Use the class with highest probability.
             correct += int((pred == data.y).sum())  # Check against ground-truth labels.
         return correct / len(loader.dataset)  # Derive ratio of correct predictions.
-        
+
     datasets = load_data()
     torch.manual_seed(12345)
     import random
     random.seed(12345)
     random.shuffle(datasets)
-    train_dataset = datasets[: int(len(datasets)*0.60) ]
-    test_dataset = datasets[int(len(datasets)*0.60):int(len(datasets)*0.80)]
+    train_dataset = datasets[: int(len(datasets) * 0.60)]
+    test_dataset = datasets[int(len(datasets) * 0.60):int(len(datasets) * 0.80)]
     random.shuffle(train_dataset)
-    train_dataset= train_dataset[0:200]
+    train_dataset = train_dataset[0:200]
 
     train_loader = DataLoader(train_dataset, batch_size=int(config["batch_size"]), shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=int(config["batch_size"]), shuffle=False)
-    model = GCN(hidden_channels=int(config["hidden"]), drop_rate = config["dropout"],\
-                hidden = int(config["hiddenlayer"]),\
-                layer_type=int(config["layer_type"]) ).float().to(device)
+    model = GCN(hidden_channels=int(config["hidden"]), drop_rate=config["dropout"],
+                hidden=int(config["hiddenlayer"]),
+                layer_type=int(config["layer_type"])).float().to(device)
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"])
 
-
     # import torch.optim.lr_scheduler as lrs
     # scheduler = lrs.ExponentialLR(optimizer, gamma=0.9)
-    for _ in range(1, 2000):     
+    for _ in range(1, 2000):
         train(model, criterion, optimizer, train_loader)
-    
+
     accu_test = test(model, test_loader)
     return accu_test
 
@@ -145,14 +147,13 @@ def run(config: dict):
 #     objective_default = run_default(default_config)
 
 
-
 def get_evaluator(run_function):
     from deephyper.evaluator.callback import LoggerCallback
     is_gpu_available = torch.cuda.is_available()
     n_gpus = torch.cuda.device_count()
     # Default arguments for Ray: 1 worker and 1 worker per evaluation
     method_kwargs = {
-        "address":'auto',
+        "address": 'auto',
         "num_cpus_per_task": 1,
         "callbacks": [LoggerCallback()]
     }
@@ -175,10 +176,10 @@ def problem():
     default_config = {
         "hiddenlayer": 2,
         "batch_size": 62,
-        "dropout":  0.4,
+        "dropout": 0.4,
         "learning_rate": 0.001,
-        "hidden":125,
-        "layer_type":0,
+        "hidden": 125,
+        "layer_type": 0,
     }
     from deephyper.problem import HpProblem
     problem = HpProblem()
@@ -198,11 +199,11 @@ def problem():
 
 
 if __name__ == "__main__":
-    from deephyper.search.hps import AMBS
     from deephyper.evaluator import Evaluator
+    from deephyper.search.hps import AMBS
     prob1 = problem()
-    evaluator_1= get_evaluator(run)
-    print("the total number of deep hyper workers are", evaluator_1.num_workers) 
+    evaluator_1 = get_evaluator(run)
+    print("the total number of deep hyper workers are", evaluator_1.num_workers)
     # Instanciate the search with the problem and a specific evaluator
     search = AMBS(prob1, evaluator_1)
     search.search(max_evals=200)
--- psd_gnn/utils.py
+++ psd_gnn/utils.py
@@ -21,6 +21,7 @@ def process_args():
                  "nowcast-clustering-16",
                  "wind-clustering-casa",
                  "wind-noclustering-casa",
+                 "1000genome_new_2022",
                  "all"
                  ]
 
@@ -65,6 +66,10 @@ def process_args():
                         type=str,
                         default=".",
                         help="Specify the root path of file.")
+    parser.add_argument("--logdir",
+                        type=str,
+                        default="runs",
+                        help="Specify the log directory.")
     parser.add_argument("--force",
                         action="store_true",
                         help="To force reprocess datasets.")
@@ -127,6 +132,26 @@ def parse_adj(workflow):
             edges.append((nodes[u], nodes[v]))
 
     return nodes, edges
+
+
+def print_dataset_info(dataset):
+    """ Print the dataset information.
+
+    Args:
+        dataset (PyG.dataset): Dataset object.
+    """
+    print(dataset)
+    print(f"dataset                 {dataset.name} \n",
+          f"# of graphs             {len(dataset)} \n",
+          f"# of graph labels       {dataset.num_classes} \n",
+          f"# of node labels        {dataset.num_node_labels} \n",
+          f"# of node features      {dataset.num_node_features} \n",
+          f"# of nodes per graph    {dataset[0].num_nodes} \n",
+          f"# of edges per graph    {dataset[0].num_edges} \n",
+          "##" * 20 + "\n"
+          )
+
+
 def create_dir(path):
     """ Create a dir where the processed data will be stored
 
@@ -257,3 +282,154 @@ def parse_data(flag, json_path, classes):
                 except BaseException:
                     print("Error with the file's {} format.".format(f))
     return graphs
+
+
+def init_model(args):
+    """ Init model for PyGOD
+
+    Args:
+        args (dict): Args from argparser.
+
+    Returns:
+        object: Model object.
+    """
+    from random import choice
+    from pygod.models import AdONE, AnomalyDAE, CONAD, DOMINANT, DONE, GAAN, GCNAE, GUIDE, MLPAE, Radar, ANOMALOUS, SCAN
+    from pyod.models.lof import LOF
+    from sklearn.ensemble import IsolationForest
+    if not isinstance(args, dict):
+        args = vars(args)
+    dropout = [0, 0.1, 0.3]
+    lr = [0.1, 0.05, 0.01]
+    weight_decay = 0.01
+
+    if args['dataset'] == 'inj_flickr':
+        # sampling and minibatch training on large dataset flickr
+        batch_size = 64
+        num_neigh = 3
+        epoch = 2
+    else:
+        batch_size = 0
+        num_neigh = -1
+        epoch = 300
+
+    model_name = args['model']
+    gpu = args['gpu']
+
+    # if hasattr(args, 'epoch'):
+    epoch = args.get('epoch', 200)
+
+    if args['dataset'] == 'reddit':
+        # for the low feature dimension dataset
+        hid_dim = [32, 48, 64]
+    else:
+        hid_dim = [32, 64, 128, 256]
+
+    if args['dataset'][:3] == 'inj':
+        # auto balancing on injected dataset
+        alpha = [None]
+    else:
+        alpha = [0.8, 0.5, 0.2]
+
+    if model_name == "adone":
+        return AdONE(hid_dim=choice(hid_dim),
+                     weight_decay=weight_decay,
+                     dropout=choice(dropout),
+                     lr=choice(lr),
+                     epoch=epoch,
+                     gpu=gpu,
+                     batch_size=batch_size,
+                     num_neigh=num_neigh)
+    elif model_name == 'anomalydae':
+        hd = choice(hid_dim)
+        return AnomalyDAE(embed_dim=hd,
+                          out_dim=hd,
+                          weight_decay=weight_decay,
+                          dropout=choice(dropout),
+                          theta=choice([10., 40., 90.]),
+                          eta=choice([3., 5., 8.]),
+                          lr=choice(lr),
+                          epoch=epoch,
+                          gpu=gpu,
+                          alpha=choice(alpha),
+                          batch_size=batch_size,
+                          num_neigh=num_neigh)
+    elif model_name == 'conad':
+        return CONAD(hid_dim=choice(hid_dim),
+                     weight_decay=weight_decay,
+                     dropout=choice(dropout),
+                     lr=choice(lr),
+                     epoch=epoch,
+                     gpu=gpu,
+                     alpha=choice(alpha),
+                     batch_size=batch_size,
+                     num_neigh=num_neigh)
+    elif model_name == 'dominant':
+        return DOMINANT(hid_dim=choice(hid_dim),
+                        weight_decay=weight_decay,
+                        dropout=choice(dropout),
+                        lr=choice(lr),
+                        epoch=epoch,
+                        gpu=gpu,
+                        alpha=choice(alpha),
+                        batch_size=batch_size,
+                        num_neigh=num_neigh)
+    elif model_name == 'done':
+        return DONE(hid_dim=choice(hid_dim),
+                    weight_decay=weight_decay,
+                    dropout=choice(dropout),
+                    lr=choice(lr),
+                    epoch=epoch,
+                    gpu=gpu,
+                    batch_size=batch_size,
+                    num_neigh=num_neigh)
+    elif model_name == 'gaan':
+        return GAAN(noise_dim=choice([8, 16, 32]),
+                    hid_dim=choice(hid_dim),
+                    weight_decay=weight_decay,
+                    dropout=choice(dropout),
+                    lr=choice(lr),
+                    epoch=epoch,
+                    gpu=gpu,
+                    alpha=choice(alpha),
+                    batch_size=batch_size,
+                    num_neigh=num_neigh)
+    elif model_name == 'gcnae':
+        return GCNAE(hid_dim=choice(hid_dim),
+                     weight_decay=weight_decay,
+                     dropout=choice(dropout),
+                     lr=choice(lr),
+                     epoch=epoch,
+                     gpu=gpu,
+                     batch_size=batch_size,
+                     num_neigh=num_neigh)
+    elif model_name == 'guide':
+        return GUIDE(a_hid=choice(hid_dim),
+                     s_hid=choice([4, 5, 6]),
+                     weight_decay=weight_decay,
+                     dropout=choice(dropout),
+                     lr=choice(lr),
+                     epoch=epoch,
+                     gpu=gpu,
+                     alpha=choice(alpha),
+                     batch_size=batch_size,
+                     num_neigh=num_neigh,
+                     cache_dir='./tmp')
+    elif model_name == "mlpae":
+        return MLPAE(hid_dim=choice(hid_dim),
+                     weight_decay=weight_decay,
+                     dropout=choice(dropout),
+                     lr=choice(lr),
+                     epoch=epoch,
+                     gpu=gpu,
+                     batch_size=batch_size)
+    elif model_name == 'lof':
+        return LOF()
+    elif model_name == 'if':
+        return IsolationForest()
+    elif model_name == 'radar':
+        return Radar(lr=choice(lr), gpu=gpu)
+    elif model_name == 'anomalous':
+        return ANOMALOUS(lr=choice(lr), gpu=gpu)
+    elif model_name == 'scan':
+        return SCAN(eps=choice([0.3, 0.5, 0.8]), mu=choice([2, 5, 10]))
